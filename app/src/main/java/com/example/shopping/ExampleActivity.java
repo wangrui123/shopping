311f9992ac373160198c89fb92f76128f2088984
wangrui <1254407151@qq.com>
2018-02-05 20:02:03 +0800

    第一次提交

--- app/src/main/java/com/example/shopping/ExampleActivity.java
+++ app/src/main/java/com/example/shopping/ExampleActivity.java
@@ -1,6 +1,10 @@
 package com.example.shopping;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import com.example.latte.activities.ProxyActivity;
+import com.example.latte.app.Latte;
 import com.example.latte.delegates.LatteDelegate;
 
 public class ExampleActivity extends ProxyActivity {
@@ -10,11 +14,11 @@ public class ExampleActivity extends ProxyActivity {
         return new ExampleDelegate();
     }
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        Toast.makeText(Latte.getApplication(), "传入context了", Toast.LENGTH_SHORT).show();
-//    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(Latte.getApplication(), "传入context了", Toast.LENGTH_SHORT).show();
+    }
 }
